390413b0e20f00f619376279c05be56da379c43f
shaunmcn2001 <219618116+shaunmcn2001@users.noreply.github.com>
2025-09-09 20:48:25 +1000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./index.css";
 import { ToastProvider } from "./components/Toast";
 
