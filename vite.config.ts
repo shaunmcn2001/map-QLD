304177121dd9a467ff669c4bbb22cfbfdaa6f1cf
shaunmcn2001 <219618116+shaunmcn2001@users.noreply.github.com>
2025-09-09 20:40:29 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
+// vite.config.ts
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
-// GitHub Pages repo name
+const isPages = process.env.BUILD_TARGET === "pages"; // pages or render
 const REPO = "map-QLD";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: { "@": resolve(__dirname, "src") }
-  },
-  // IMPORTANT for GitHub Project Pages
-  base: `/${REPO}/`,
+  resolve: { alias: { "@": resolve(__dirname, "src") } },
+  base: isPages ? `/${REPO}/` : "/", // Render -> "/", GH Pages -> "/map-QLD/"
 });
